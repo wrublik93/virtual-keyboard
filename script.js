393557ffb2a0002fddd0d5452388e3e6df4f469e
wrublik93 <wrublik.93@mail.ru>
2020-04-05 03:33:44 +0300

    feat: implement function for caps with shift keys

--- script.js
+++ script.js
@@ -26,6 +26,13 @@ const keyboardKeys = {
       'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter',
       '&#8679; Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '&#9650;', 'Shift &#8679;',
       'Ctrl', 'Win', 'Alt', ' ', 'Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl'],
+    
+    capsShiftKeys: ['ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', '&#8592;',
+      'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '/', 'Del',
+      'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
+      '&#8679; Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', ',', '&#9650;', 'Shift &#8679;',
+      'Ctrl', 'Win', 'Alt', ' ', 'Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl'],
+    
   },
   en: {
     nameKeys: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '&#8592;',
@@ -45,16 +52,26 @@ const keyboardKeys = {
       'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter',
       '&#8679; Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '&#9650;', 'Shift &#8679;',
       'Ctrl', 'Win', 'Alt', ' ', 'Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl'],
+    
+    capsShiftKeys: ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '&#8592;',
+      'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', '|', 'Del',
+      'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', 'Enter',
+      '&#8679; Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', '&#9650;', 'Shift &#8679;',
+      'Ctrl', 'Win', 'Alt', ' ', 'Alt', '&#9668;', '&#9660;', '&#9658;', 'Ctrl'],
   },
 };
+
+
 
 const { codeKeys } = keyboardKeys;
 const nameKeysRu = keyboardKeys.ru.nameKeys;
 const capsKeysRu = keyboardKeys.ru.capsKeys;
 const shiftKeysRu = keyboardKeys.ru.shiftKeys;
+const capsShiftKeysRu = keyboardKeys.ru.capsShiftKeys;
 const nameKeysEn = keyboardKeys.en.nameKeys;
 const capsKeysEn = keyboardKeys.en.capsKeys;
 const shiftKeysEn = keyboardKeys.en.shiftKeys;
+const capsShiftKeysEn = keyboardKeys.en.capsShiftKeys;
 
 function initKeyboard(code, nameKeys) {
   let out = '';
@@ -107,11 +124,22 @@ document.addEventListener('keydown', (event) => {
       document.querySelector('.virtual-keyboard .k-key[data="CapsLock"]').classList.add('transform');
       document.querySelector('.virtual-keyboard .k-key[data="CapsLock"]').classList.remove('press-key');
     } else if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
-      if (!localStorage.getItem('lang') || localStorage.getItem('lang') === 'ru') {
-        initKeyboard(codeKeys, shiftKeysRu);
-      } else if (localStorage.getItem('lang') === 'en') {
-        initKeyboard(codeKeys, shiftKeysEn);
+      if(localStorage.getItem('caps') === 'false') {
+        if (!localStorage.getItem('lang') || localStorage.getItem('lang') === 'ru') {
+          initKeyboard(codeKeys, shiftKeysRu);
+        } else if (localStorage.getItem('lang') === 'en') {
+          initKeyboard(codeKeys, shiftKeysEn);
+        }
+      } else if(localStorage.getItem('caps') === 'true') {
+        if (!localStorage.getItem('lang') || localStorage.getItem('lang') === 'ru') {
+          initKeyboard(codeKeys, capsShiftKeysRu);
+          document.querySelector('.virtual-keyboard .k-key[data="CapsLock"]').classList.add('press-key');
+        } else if (localStorage.getItem('lang') === 'en') {
+          initKeyboard(codeKeys, capsShiftKeysEn);
+          document.querySelector('.virtual-keyboard .k-key[data="CapsLock"]').classList.add('press-key');
+        }
       }
+      
       document.querySelector(`.virtual-keyboard .k-key[data="${event.code}"]`).classList.add('active');
       document.querySelector(`.virtual-keyboard .k-key[data="${event.code}"]`).classList.add('transform');
     }
@@ -121,10 +149,20 @@ document.addEventListener('keydown', (event) => {
 document.addEventListener('keyup', (event) => {
   if (codeKeys.includes(event.code)) {
     if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
-      if (!localStorage.getItem('lang') || localStorage.getItem('lang') === 'ru') {
-        initKeyboard(codeKeys, nameKeysRu);
-      } else if (localStorage.getItem('lang') === 'en') {
-        initKeyboard(codeKeys, nameKeysEn);
+      if(localStorage.getItem('caps') === 'false') {
+        if (!localStorage.getItem('lang') || localStorage.getItem('lang') === 'ru') {
+          initKeyboard(codeKeys, nameKeysRu);
+        } else if (localStorage.getItem('lang') === 'en') {
+          initKeyboard(codeKeys, nameKeysEn);
+        }
+      } else if(localStorage.getItem('caps') === 'true') {
+        if (!localStorage.getItem('lang') || localStorage.getItem('lang') === 'ru') {
+          initKeyboard(codeKeys, capsKeysRu);
+          document.querySelector('.virtual-keyboard .k-key[data="CapsLock"]').classList.add('press-key');
+        } else if (localStorage.getItem('lang') === 'en') {
+          initKeyboard(codeKeys, capsKeysEn);
+          document.querySelector('.virtual-keyboard .k-key[data="CapsLock"]').classList.add('press-key');
+        }
       }
     }
     document.querySelector(`.virtual-keyboard .k-key[data="${event.code}"]`).classList.remove('active');
